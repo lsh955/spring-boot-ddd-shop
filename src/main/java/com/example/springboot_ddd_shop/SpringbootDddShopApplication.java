25f59e45f12745a899c4be2906777b959f4dda5f
이승환 <lsh01087674117@gmail.com>
2022-12-06 18:22:27 +0900

    초기 프로젝트 생성

---
+++ src/main/java/com/example/springboot_ddd_shop/SpringbootDddShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ddd_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDddShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDddShopApplication.class, args);
+    }
+
+}
